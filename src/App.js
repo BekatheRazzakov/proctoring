4044936d8ddf23eb846603b929d54e4499d21016
Bektur Razzakov <brzzkv@gmail.com>
2024-05-11 19:49:02 +0600

    Face detector

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Webcam from 'react-webcam';
+import { useFaceDetection} from 'react-use-face-detection';
+import FaceDetection from '@mediapipe/face_detection';
+import {Camera} from '@mediapipe/camera_utils';
 
-function App() {
+const width = 500;
+const height = 500;
+
+const App = () => {
+  const {webcamRef, boundingBox, detected, facesDetected} = useFaceDetection({
+    faceDetectionOptions: {
+      model: 'short',
+    },
+    faceDetection: new FaceDetection.FaceDetection({
+      locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection/${file}`,
+    }),
+    camera: ({mediaSrc, onFrame}) =>
+      new Camera(mediaSrc, {
+        onFrame,
+        width,
+        height,
+      }),
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{`Face Detected: ${detected}`}</p>
+      <p>{`Number of faces detected: ${facesDetected}`}</p>
+      <div style={{width, height, position: 'relative'}}>
+        {boundingBox.map((box, index) => (
+          <div
+            key={`${index + 1}`}
+            style={{
+              border: '4px solid red',
+              position: 'absolute',
+              top: `${box.yCenter * 100}%`,
+              left: `${box.xCenter * 100}%`,
+              width: `${box.width * 100}%`,
+              height: `${box.height * 100}%`,
+              zIndex: 1,
+            }}
+          />
+        ))}
+        <Webcam
+          ref={webcamRef}
+          forceScreenshotSourceSize
+          style={{
+            height,
+            width,
+            position: 'absolute',
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
